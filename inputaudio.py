d5517173e48d45a3f9167768ffd07421316aff22
awong15 <Alycia.Wong@tufts.edu>
2019-04-18 20:09:19 -0400

    Changed graph a bit
    * Changed "Int16" to "Int 64" and re-calibrated graph
    * Changed line 22 from "np.fromstring" to "np.frombuffer"
    * Changed font and graph sizes
    * Added comments to the top of each function
    * Commented out unused modules

--- inputaudio.py
+++ inputaudio.py
@@ -8,35 +8,42 @@ Created on Thu Apr 18 17:13:20 2019
 
 import scipy.signal as sig
 import matplotlib.pyplot as plt
-import plotly.plotly as ply
+# import plotly.plotly as ply
 import numpy as np
 import wave
 
+# Imports a sound file and extracts raw audio data (amp) and frames per second
+# (fs) and convert frames per second into number of seconds elapsed (Time).
+# Returns the y-value as a numpy array of int64, number of frames per second, 
+# and amount of time elapsed
 def readData():
-    spf = wave.open('PianoF.wav', 'r') #import sound file
-    #Extract Raw Audio from Wav File
+    spf = wave.open("PianoF.wav", 'r')
     amp = spf.readframes(-1)
-    amp = np.fromstring(amp, 'Int16')
-    fs = spf.getframerate() #frames per second
-    #Convert Frames to Time
+    amp = np.frombuffer(amp, "Int64")
+    fs = spf.getframerate()
     time = np.linspace(0, len(amp)/fs, num=len(amp))
     return (amp, fs, time)
-(amp, fs, time) = readData()
 
+# Plot a time vs amp graph that is zoomed in to see the individual waves
 def plotData(time, amp):
-    plt.figure(1)
-    plt.title('Amplitude Wave')
+    plt.figure(figsize = (20, 10))
     plt.plot(time, amp)
-    plt.xlim(2,2.05) #Adjust limits of the x-axis
-    plt.ylim(-3000,3000) #Adjust limits of the y-axis
+    plt.title("Time vs Pressure", fontsize = 30)
+    plt.xlabel("Time", fontsize = 24)
+    plt.ylabel("Volts/Pressure", fontsize = 30)
+    plt.xticks(fontsize = 18)
+    plt.yticks(fontsize = 18)
+    plt.xlim(0.6, 0.61) #Adjust limits of the x-axis
+    plt.ylim(-3 * 10**17, 3 * 10**17) #Adjust limits of the y-axis
     plt.grid()
     plt.show()
+
+# def fourier(amp, fs, time):
+    # ts = amp / fs
+    # ff = 
+
+(amp, fs, time) = readData()
 plotData(time, amp)
-
-def fourier(amp, fs, time):
-    ts = amp / fs
-    ff = 
-
 
 ######
 """from scipy.io.wavfile import read
